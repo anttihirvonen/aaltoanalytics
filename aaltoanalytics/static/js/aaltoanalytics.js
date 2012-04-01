85d207715b3c9fb0326a4bff73d253faa718284c
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-04-01 15:40:14 +0300

    Better handling for multiple tracked parameters + added some stubs.

--- aaltoanalytics/static/js/aaltoanalytics.js
+++ aaltoanalytics/static/js/aaltoanalytics.js
@@ -1,6 +1,9 @@
 /*
  * Aalto Analytics tracking script.
  *
+ * This script should be loaded in head-section of the page.
+ * Call AaltoAnalytics.trackPageview() in the end of the body
+ * tag to actually track a pageview.
  */
 
 var AaltoAnalytics;
@@ -22,14 +25,29 @@ if(!this.AaltoAnalytics) {
             image.src = trackerUrl + request;
         }
         
-        this.trackPageView = function() {
-            /*
-             * Does all the heavy lifting and actual tracking of page views
-             */
-            var width = screen.width;
-            var height = screen.height;
+        function getUserId() {
+            // Implement
+        }
 
-            getImage("?swidth="+width+"&sheight="+height);
+        function getVisitId() {
+            // Implement
+        }
+        
+        /*
+         * Does all the heavy lifting and actual tracking of page views
+         */
+        this.trackPageView = function() {
+            params = {}
+            params['swidth'] = screen.width;
+            params['sheight'] = screen.height;
+
+            // Construct GET-parameter string from collected values
+            paramArray = []
+            for(var key in params){ paramArray.push(key+'='+params[key]); }
+            getParams = paramArray.join("&")
+
+            // Actual request
+            getImage("?"+getParams);
         }
     }
 }
