eb2f1deed0f08fde1ae311cb634f24d66f6d9b7b
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-05-07 20:00:04 +0300

    Refactoring mobile views finished.
    Templates moved + urls renamed.

--- aaltoanalytics/apps/mobile/urls.py
+++ aaltoanalytics/apps/mobile/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('aaltoanalytics.apps.mobile.views',
-    url(r'^$', 'mobile_index', name="analytics-mobile_index"),
-    url(r'^content/hot/$', 'mobile_hot_content', name="analytics-mobile_hot_content"),
-    url(r'^content/mostviewed/$', 'mobile_most_viewed_content', name="analytics-mobile_most_viewed_content"),
-    url(r'^users/activeperservice/$', 'mobile_active_users_per_service', name="analytics-mobile_active_users_per_service"),
+    url(r'^$', 'mobile_index', name="mobile-index"),
+    url(r'^content/hot/$', 'mobile_hot_content', name="mobile-hot_content"),
+    url(r'^content/mostviewed/$', 'mobile_most_viewed_content', name="mobile-most_viewed_content"),
+    url(r'^users/activeperservice/$', 'mobile_active_users_per_service', name="mobile-active_users_per_service"),
 )
--- aaltoanalytics/apps/mobile/views.py
+++ aaltoanalytics/apps/mobile/views.py
@@ -15,7 +15,7 @@ def active_time_limit(hours=1):
 def mobile_index(request):
     # pick only users that have been active in past hour
     active_users = Pageview.objects.values('user_id').filter(datetime__gte=active_time_limit()).distinct().count()
-    return render(request, 'analytics/mobile/index.html', {'active_users' : active_users })
+    return render(request, 'mobile/index.html', {'active_users' : active_users })
 
 def mobile_hot_content(request):
     service_pageview_list = []
@@ -23,7 +23,7 @@ def mobile_hot_content(request):
     # TODO: formulate into one query
     for service in Service.objects.all():
         service_pageview_list.append({'service' : service, 'pageviews' : Pageview.objects.filter(service=service, datetime__gte=active_time_limit(hours)).values('url', 'title').annotate(Avg("total_read_time")).order_by("-total_read_time__avg")})
-    return render(request, 'analytics/mobile/hot_content.html', {'service_pageview_list' : service_pageview_list })
+    return render(request, 'mobile/hot_content.html', {'service_pageview_list' : service_pageview_list })
 
 def mobile_most_viewed_content(request):
     service_pageview_list = []
@@ -31,7 +31,7 @@ def mobile_most_viewed_content(request):
     # TODO: formulate into one query
     for service in Service.objects.all():
         service_pageview_list.append({'service' : service, 'pageviews' : Pageview.objects.filter(service=service, datetime__gte=active_time_limit(hours)).values('url', 'title').annotate(Count("url")).order_by("-url__count")})
-    return render(request, 'analytics/mobile/most_viewed_content.html', {'service_pageview_list' : service_pageview_list })
+    return render(request, 'mobile/most_viewed_content.html', {'service_pageview_list' : service_pageview_list })
 
 def mobile_active_users_per_service(request):
     hours = request.GET.get('hours', 1)
@@ -41,7 +41,7 @@ def mobile_active_users_per_service(request):
     for service in services:
         service.users = Pageview.objects.values('user_id').filter(service=service, datetime__gte=active_time_limit(hours)).distinct().count()
 
-    return render(request, 'analytics/mobile/active_users_per_service.html', {'services' : services})
+    return render(request, 'mobile/active_users_per_service.html', {'services' : services})
 
 def developer_view(request):
     pass
