63f4475c56817d766f99b9f2ba636eb3d6edd4aa
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-05-07 20:25:10 +0300

    .dev subdomain configured to serve develoment views.

--- aaltoanalytics/hosts.py
+++ aaltoanalytics/hosts.py
@@ -4,6 +4,6 @@ from django.conf import settings
 host_patterns = patterns('',
     # Not really mobile views, but for testing..
     host(r'm', 'aaltoanalytics.apps.mobile.urls', name='mobile'),
-    host(r'dev', 'aaltoanalytics.apps.analytics.urls', name='development'),
+    host(r'dev', 'aaltoanalytics.apps.development.urls', name='development'),
     host(r'www', settings.ROOT_URLCONF, name="normal")
 )
