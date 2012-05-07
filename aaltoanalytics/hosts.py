73878ad88f81dbd94961ab3019a5b86655feb874
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-05-07 20:09:46 +0300

    m. subdomain serves now correct mobile views

--- aaltoanalytics/hosts.py
+++ aaltoanalytics/hosts.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 host_patterns = patterns('',
     # Not really mobile views, but for testing..
-    host(r'm', 'aaltoanalytics.apps.analytics.urls', name='mobile'),
+    host(r'm', 'aaltoanalytics.apps.mobile.urls', name='mobile'),
     host(r'dev', 'aaltoanalytics.apps.analytics.urls', name='development'),
     host(r'www', settings.ROOT_URLCONF, name="normal")
 )
