5ea51465a664002640d4b9c0b7cd3600a0c3692f
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-05-20 11:57:04 +0300

    Development/views.py cleanup/comments.

--- aaltoanalytics/apps/development/views.py
+++ aaltoanalytics/apps/development/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
 from aaltoanalytics.apps.analytics.models import Pageview
@@ -7,6 +6,12 @@ from django.db.models import Count, Avg
 from datetime import datetime, timedelta
 
 def development_index(request):
+    """
+    Shows the stats that are interesting to developers. 
+    
+    Currently includes only browser and operating system 
+    marketshare.
+    """
     start_date = request.GET.get("start_date", "")
     start_date = datetime.strptime(start_date, "%d%m%y") if start_date else datetime.utcnow() - timedelta(days=7)
  
@@ -14,11 +19,14 @@ def development_index(request):
     end_date = datetime.strptime(end_date, "%d%m%y") if end_date else datetime.utcnow()
     
     # Since sqlite doesn't seem to support distinct in certain cases, this is a hack
-    # -- works as a quick hack, but better solution needed..
+    # -- works for now, but a better solution would be nice..
     browsers = Pageview.objects.filter(datetime__gte=start_date, datetime__lte=end_date).values('browser_name').distinct()
     for b in browsers:
         b['user_count'] = Pageview.objects.filter(datetime__gte=start_date, datetime__lte=end_date, browser_name=b['browser_name']).values('user_id').distinct().count()
     return render(request, 'development/index.html', {'start_date' : start_date, 'end_date' : end_date, 'browsers' : browsers })
     
 def development_show_raw_log(request):
+    """
+    Shows the raw query log. Includes all the logged pageviews.
+    """
     return render(request, 'analytics/show.html', {'pageviews' : Pageview.objects.all() })
